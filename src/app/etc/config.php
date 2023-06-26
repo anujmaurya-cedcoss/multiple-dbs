d808ad541f952206b58033bbda1da2459b3f1181
Anuj Maurya <anujmaurya@cedcoss.com>
2023-06-26 12:36:13 +0530

    initial commit

---
+++ src/app/etc/config.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace MyApp\Config;
+
+return[
+    'app'=>[
+        'name'=>"MyApp",
+        'version'=>"4.1.12",
+    ],
+    'db1'=>[
+        'host'=>'mysql-server',
+        'username' => 'root',
+        'password' => 'secret',
+        'dbname'   => 'db1',
+    ],
+    'db2'=> [
+        'host'=>'mysql-server',
+        'username' => 'root',
+        'password' => 'secret',
+        'dbname'   => 'db2',
+    ]
+];
